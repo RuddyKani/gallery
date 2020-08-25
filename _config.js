0322ca1047c90d7bdf0759581e460da143e1820d
Purity Mukami <ruddymukamip@gmail.com>
2020-08-25 21:31:18 +0300

    Corrected URI mongoDB

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<ruddy>:<ruddy123>@clustergallery.37gwb.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<ruddy>:<ruddy123>@clustergallery.37gwb.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<ruddy>:<ruddy123>@clustergallery.37gwb.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://ruddy:ruddy123@clustergallery.37gwb.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://ruddy:ruddy123@clustergallery.37gwb.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://ruddy:ruddy123@clustergallery.37gwb.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
